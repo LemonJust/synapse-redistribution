676d0becef1ac430699dd123f82c195785b97e36
LemonJust <nadtochi@usc.edu>
2020-03-31 18:02:01 -0700

    remove voxels from initial data import

--- get_data/utils/getCohortData.m
+++ get_data/utils/getCohortData.m
@@ -2,9 +2,6 @@ function [stu,syn] = getCohortData(in)
 %% get input
 cohort_path = in.cohort_path;
 cohort_key = in.key_path;
-% image_file = in.image_file;
-% image_res = in.image_res;
-% xyzMicronStep = in.voxel_side;
 template_midplane = in.template_midplane;
 yzplane_points = in.yzplane_points;
 template_midplane_res = in.template_midplane_res;
@@ -24,15 +21,6 @@ stu = getHowSmartIdV2(cohort_key,unique_id);
 [lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter] = getxyz(table,...
     nStudies,lostId,gainedId,unchangedId);
 
-%%
-% Img = getTiffInfo(image_file);
-% Voxelize to image
-% xyzMicronRange = [Img.width Img.height Img.nFrame].*image_res;
-% voxelRange = floor(xyzMicronRange/xyzMicronStep)+1;
-% 
-% [lost,gained,unchangedBefore,unchangedAfter,allBefore,...
-%     allAfter] = getVoxelData(voxelRange,xyzMicronStep,lost,gained,...
-%     unchangedBefore,unchangedAfter,allBefore,allAfter);
 %% get xyz coordinates for all the synapses in the Dorsal space 
 % apply midplane transform : 
 % dorsalize by setting midplane of the fish to be Oyz
@@ -40,23 +28,6 @@ stu = getHowSmartIdV2(cohort_key,unique_id);
     getxyzAnat(lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter,...
     template_midplane,yzplane_points,...
     template_midplane_res,yzplane_res);
-%% get Voxel information in the Dorsal space 
-% [lost,gained,unchangedBefore,unchangedAfter,allBefore,...
-%     allAfter] = getVoxelDataAnat(voxelRange,xyzMicronStep,lost,gained,...
-%     unchangedBefore,unchangedAfter,allBefore,allAfter);
-
-%% get number of fish in each voxel (occupancy)
-% calculates how many different FISH contribute their synapses to 
-% a particular voxel 
-
-% in the Standart Fish Space
-% [lost,gained,unchangedBefore,unchangedAfter,allBefore,...
-%     allAfter] = getOccupancy(0,lost,gained,...
-%     unchangedBefore,unchangedAfter,allBefore,allAfter);
-% % in the Dorsal space 
-% [lost,gained,unchangedBefore,unchangedAfter,allBefore,...
-%     allAfter] = getOccupancyAnat(0,lost,gained,unchangedBefore,...
-%     unchangedAfter,allBefore,allAfter);
 
 %% form a structure 
 syn.lost = lost;
