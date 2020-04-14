7e8c167652301907f98124acc9aade87204dd140
LemonJust <nadtochi@usc.edu>
2020-04-13 19:58:14 -0700

    add data import for single entry
    NEW : can turn off dorsalization ,  unassigned stu field , remove empty fields

--- get_data/utils/getCohortData.m
+++ get_data/utils/getCohortData.m
@@ -2,34 +2,46 @@ function [stu,syn] = getCohortData(in)
 %% get input
 cohort_path = in.cohort_path;
 cohort_key = in.key_path;
-template_midplane = in.template_midplane;
-yzplane_points = in.yzplane_points;
-template_midplane_res = in.template_midplane_res;
-yzplane_res = in.yzplane_res;
+
+if isfield(in, 'dorsalize')
+    dorsalize = in.dorsalize;
+else
+    dorsalize = true;
+end
+
 %% read cohort
 table = readtable(cohort_path);
 %% sort studies into Learners/Nonlearners/CStim/NStim/UStim
 unique_id = strtrim(unique(table.study_id));
 nStudies = length(unique_id);
 
-stu = getHowSmartIdV2(cohort_key,unique_id);
+% TODO : make cohort_key = [] be an option
 
-%% get indices of lost/gained/unchanged synapses for each study 
+stu = getHowSmartIdV2(cohort_key,unique_id);
+%% get indices of lost/gained/unchanged synapses for each study
 [unchangedId,lostId,gainedId] = getStudiesV2(table,unique_id);
 
 %% get xyz coordinates for all the synapses in the Standart Fish Space
 [lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter] = getxyz(table,...
     nStudies,lostId,gainedId,unchangedId);
 
-%% get xyz coordinates for all the synapses in the Dorsal space 
-% apply midplane transform : 
+%% get xyz coordinates for all the synapses in the Dorsal space
+% apply midplane transform :
 % dorsalize by setting midplane of the fish to be Oyz
-[lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter] = ...
-    getxyzAnat(lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter,...
-    template_midplane,yzplane_points,...
-    template_midplane_res,yzplane_res);
+if dorsalize
+    
+    template_midplane = in.template_midplane;
+    yzplane_points = in.yzplane_points;
+    template_midplane_res = in.template_midplane_res;
+    yzplane_res = in.yzplane_res;
+    
+    [lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter] = ...
+        getxyzAnat(lost,gained,unchangedBefore,unchangedAfter,allBefore,allAfter,...
+        template_midplane,yzplane_points,...
+        template_midplane_res,yzplane_res);
+end
 
-%% form a structure 
+%% form a structure
 syn.lost = lost;
 syn.gained = gained;
 syn.unchangedBefore = unchangedBefore;
@@ -37,10 +49,12 @@ syn.unchangedAfter = unchangedAfter;
 syn.allBefore = allBefore;
 syn.allAfter = allAfter;
 
-%% remove synapses on the other side of the midplane 
-% plane YZ coordinates 
-midplanePoints = readtable(in.yzplane_points);
-midplaneX = midplanePoints.X(1)*in.yzplane_res(1);
-syn = removewrongside(syn,midplaneX);
+%% remove synapses on the other side of the midplane
+% plane YZ coordinates
+if dorsalize
+    midplanePoints = readtable(yzplane_points);
+    midplaneX = midplanePoints.X(1)*yzplane_res(1);
+    syn = removewrongside(syn,midplaneX);
+end
 
 end
--- get_data/utils/getHowSmartIdV2.m
+++ get_data/utils/getHowSmartIdV2.m
@@ -3,45 +3,113 @@ function stu = getHowSmartIdV2(cohort_key,unique_id)
 % returns the studies id (iStudy set) corresponding to
 % Learners, Nonlearners, USTim, CStim
 
-unique_id = strtrim(unique_id);
+% unique_id = strtrim(unique_id);
 key = readtable(cohort_key);
-%%
+
 study_type = strtrim(key.study_type);
 study_id = strtrim(key.study_id);
-%%
+%% split into behavioural groups
+% Learners_name contains only the names of learners, Nonlearners_name -
+% nonlearners and etc
+
+% TODO : replace it by group_name from get_group
 Learners_name = study_id(ismember(study_type, 'Learner'));
 Nonlearners_name = study_id(ismember(study_type, 'Nonlearner'));
 NS_name = study_id(ismember(study_type, 'No Stim Control'));
 US_name = study_id(ismember(study_type, 'US Only Control'));
 CS_name = study_id(ismember(study_type, 'CS Only Control'));
 
-%%
+%% Search the manes in csv for the enties in the key
+all_found = [];
+% TODO : make this a function call
 [stu.Learners,is_in_key] = get_group(study_id,study_type,...
     'Learner',unique_id);
-stu.Id.Learners = Learners_name(is_in_key);
+% count how many fish has been found
+n_in_key = sum(is_in_key);
+disp([num2str(sum(is_in_key)),' Learners']);
+% finalise creating entry if there are any
+if sum(is_in_key)>0
+    stu.Id.Learners = Learners_name(is_in_key);
+    all_found = [all_found,stu.Learners];
+else
+    stu = rmfield(stu,'Learners');
+end
 
 [stu.Nonlearners,is_in_key] = get_group(study_id,study_type,...
     'Nonlearner',unique_id);
-stu.Id.Nonlearners = Nonlearners_name(is_in_key);
+% count how many fish has been found
+n_in_key = n_in_key + sum(is_in_key);
+disp([num2str(sum(is_in_key)),' Nonlearners']);
+% finalise creating entry if there are any
+if sum(is_in_key)>0
+    stu.Id.Nonlearners = Nonlearners_name(is_in_key);
+    all_found = [all_found,stu.Nonlearners];
+else
+    stu = rmfield(stu,'Nonlearners');
+end
 
 [stu.NStim,is_in_key] = get_group(study_id,study_type,...
     'No Stim Control',unique_id);
-stu.Id.NStim = NS_name(is_in_key);
+% count how many fish has been found
+n_in_key = n_in_key + sum(is_in_key);
+disp([num2str(sum(is_in_key)),' No Stim Controls']);
+% finalise creating entry if there are any
+if sum(is_in_key)>0
+    stu.Id.NStim = NS_name(is_in_key);
+    all_found = [all_found,stu.NStim];
+else
+    stu = rmfield(stu,'NStim');
+end
 
 [stu.UStim,is_in_key] = get_group(study_id,study_type,...
     'US Only Control',unique_id);
-stu.Id.UStim = US_name(is_in_key);
+% count how many fish has been found
+n_in_key = n_in_key + sum(is_in_key);
+disp([num2str(sum(is_in_key)),' US Only Controls']);
+% finalise creating entry if there are any
+if sum(is_in_key)>0
+    stu.Id.UStim = US_name(is_in_key);
+    all_found = [all_found,stu.UStim];
+else
+    stu = rmfield(stu,'UStim');
+end
 
 [stu.CStim,is_in_key] = get_group(study_id,study_type,...
     'CS Only Control',unique_id);
-stu.Id.CStim = CS_name(is_in_key);
+% count how many fish has been found
+n_in_key = n_in_key + sum(is_in_key);
+disp([num2str(sum(is_in_key)),' CS Only Controls']);
+% finalise creating entry if there are any
+if sum(is_in_key)>0
+    stu.Id.CStim = CS_name(is_in_key);
+    all_found = [all_found,stu.CStim];
+else
+    stu = rmfield(stu,'CStim');
+end
+
+
+%% See if there are any unassigned fish
+n_in_table = length(unique_id);
+n_unasgn = n_in_table - n_in_key;
+disp([num2str(n_unasgn),' Unassigned']);
+if n_unasgn>0
+   
+    % create unassigned group
+    stu.Unassigned = 1:n_in_table;
+    stu.Unassigned(all_found) = [];
+    stu.Id.Unassigned = unique_id(stu.Unassigned);
+    
+end
+
+
 end
 %% HELPER FUNCTIONS:
 function [group_members,is_in_key] = get_group(study_id,study_type,...
     how_smart,unique_id)
-% since key file can contain info about more fish than currently considered
-% in the cohort, we take all the fish in the key for that group and search
+% key file can contain info about more fish than currently considered
+% we take all the fish in the KEY for that group and search
 % for them in the current cohort
+% if the fish is there - remember the number of that fish
 
 group_name = study_id(ismember(study_type, how_smart));
 
